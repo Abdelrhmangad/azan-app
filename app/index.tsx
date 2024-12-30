cf0fabd479e8c82444177d159d6b6d09a6379058
Abdelrhman Gad <abdelrhmangad114@gmail.com>
2024-12-30 14:28:29 +0200

    last

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+    return (
+        <View className='flex flex-1 items-center justify-center bg-red-500'>
+            <Text>Aora</Text>
+            <Link href={"/profile"}>
+                Go to profile
+            </Link>
+        </View>
+    )
+}
+
+export default index
