31753e785db0ab09ba63c803839558e16452b2a8
abdelrhmangad <abdelrhmangad114@gmail.com>
2024-12-30 16:32:48 +0200

    add fonts to the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-
+import { StatusBar } from 'expo-status-bar'
 const index = () => {
     return (
-        <View className='flex flex-1 items-center justify-center bg-red-200'>
-            <Text>Aora</Text>
-            <Link href={"/profile"}>
+        <View className='flex flex-1 items-center justify-center bg-white'>
+            <Text className='text-3xl'>Aora!</Text>
+            <Link href={"/profile"} className='text-blue-500'>
                 Go to profile
             </Link>
+            <StatusBar style="dark" />
         </View>
     )
 }
