a44b7a3fc12e3262c01edba61ea0264c97912a0c
abdelrhmangad <abdelrhmangad114@gmail.com>
2025-01-07 17:57:13 +0200

    made custom to get current time

--- hooks/useCurrentTime.js
+++ hooks/useCurrentTime.js
@@ -2,33 +2,33 @@
 import { useState, useEffect } from 'react';
 
 const useCurrentTime = () => {
-  const [currentTime, setCurrentTime] = useState('');
-  const [hours, setHours] = useState('');
-  const [minutes, setMinutes] = useState('');
-  const [amPm, setAmPm] = useState('');
+    const [currentTime, setCurrentTime] = useState('');
+    const [hours, setHours] = useState('');
+    const [minutes, setMinutes] = useState('');
+    const [amPm, setAmPm] = useState('');
 
-  useEffect(() => {
-    const updateTime = () => {
-      const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
-      const amPm = hours >= 12 ? 'PM' : 'AM';
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date();
+            const hours = now.getHours();
+            const minutes = now.getMinutes();
+            const amPm = hours >= 12 ? 'PM' : 'AM';
 
-      setCurrentTime(`${hours % 12 || 12}:${minutes.toString().padStart(2, '0')} ${amPm}`);
-      setHours(hours);
-      setMinutes(minutes);
-      setAmPm(amPm);
+            setCurrentTime(`${hours % 12 || 12}:${minutes.toString().padStart(2, '0')} ${amPm}`);
+            setHours(hours);
+            setMinutes(minutes);
+            setAmPm(amPm);
 
-    };
+        };
 
-    const timer = setInterval(updateTime, 1000);
+        const timer = setInterval(updateTime, 1000);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
 
-  return {currentTime, hours, minutes, amPm};
+    return { currentTime, hours, minutes, amPm };
 };
 
 export default useCurrentTime;
